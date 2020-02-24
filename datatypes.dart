c857d97d938bb73d3d6c26e4cb626343b1d40e26
ysandeepguptha3 <59405559+ysandeepguptha3@users.noreply.github.com>
2020-02-24 23:32:06 +0530

    datatypes and ways to declare variables

---
+++ datatypes.dart
@@ -0,0 +1,22 @@
+void main() {
+  print("Yelgoi Sandeep Guptha");
+  //There are int , float , double , String , char
+
+  //There are four types of variable declarations
+  //1.
+  String v = "Yelgoi sandeep guptha";
+  //2
+  var v1 = "sandep";
+  var v2 = 10;
+  var v3 = 56.5;
+  print(v1);
+  print(v2);
+  print(v3);
+  //3
+  const c = 10;
+  print(c);
+  //4
+  dynamic v5;
+  v5 = 100;
+  print(v5);
+}
