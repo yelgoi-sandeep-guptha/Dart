0002be95674dcdcb8fde33db89364ad720b51451
ysandeepguptha3 <59405559+ysandeepguptha3@users.noreply.github.com>
2020-02-29 00:15:22 +0530

    Create ForLoop.dart

---
+++ ForLoop.dart
@@ -0,0 +1,13 @@
+void main(){
+  int v=5;
+  for(var i=0;i<v;i++){
+    print("Sandeep");
+  }
+  for(var i in [1,2,3])
+  {
+    print(i);
+  }
+  print("YSG");
+  var s=[1,2,3,4];
+  s.forEach((i)=>print(i*5));
+}
